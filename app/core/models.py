6f076dc251a89a28f0f99b2d47899fd900b9e10f
zolejester <zole.jester@gmail.com>
2024-03-11 01:34:40 +0530

    Created User Model & Setup Django Admin

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,46 @@
-from django.db import models  # noqa
+"""
+Database model
+"""
+from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+# AbstractBaseUser provides functios for authentication
+# PermissionsMixin provides functionality for permissions in django
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user"""
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'

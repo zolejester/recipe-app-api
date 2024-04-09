5d48e9d9debe7a7f260c10b2e9f397e3100494bf
zolejester <48050921+zolejester@users.noreply.github.com>
2024-04-10 00:08:25 +0530

    Update test_models.py

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,8 +7,7 @@ class ModelTests(TestCase):
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
-            email=email,
-            password=password,
+            email=email, password=password, 
         )
 
         self.assertEqual(user.email, email)
@@ -17,10 +16,10 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """Test email is normalized for new user"""
         sample_emails = [
-            ['test1@EXAMPLE.com', 'test1@example.com'], 
-            ['Test2@Example.com', 'Test2@example.com'], 
-            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'], 
-            ['test4@example.COM', 'test4@example.com'], 
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')

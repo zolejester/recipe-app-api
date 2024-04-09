3cca0762a18e18e6ca64c3631ba5702c4a333494
zolejester <zole.jester@gmail.com>
2024-04-10 02:44:00 +0530

    conflicts fixed with tag and recipe

---
+++ app/core/tests/test_models_BACKUP_471.py
@@ -0,0 +1,88 @@
+"""
+Tests for models
+"""
+from decimal import Decimal
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from core import models
+
+
+def create_user(email='user@example.com', password='testpass123'):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email, password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new user"""
+        sample_emails = [
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 4d4c813886189e35275cbeb6193e69619c0a8e6e
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+<<<<<<< HEAD
+=======
+            ['test1@EXAMPLE.com','test1@example.com'], 
+            ['Test2@Example.com','Test2@example.com'], 
+            ['TEST3@EXAMPLE.COM','TEST3@example.com'], 
+            ['test4@example.COM','test4@example.com'], 
+>>>>>>> f7f2c75808d64ff3a4735cad53d7658e253a7e12
+=======
+>>>>>>> 4d4c813886189e35275cbeb6193e69619c0a8e6e
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test Creating a super user"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123'
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipe description',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)

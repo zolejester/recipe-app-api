4e4a13cad5968813977963719d8cb9ea7b99f536
zolejester <48050921+zolejester@users.noreply.github.com>
2024-04-10 00:04:17 +0530

    Update test_models.py

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -17,10 +17,10 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """Test email is normalized for new user"""
         sample_emails = [
-            ['test1@EXAMPLE.com','test1@example.com'], 
-            ['Test2@Example.com','Test2@example.com'], 
-            ['TEST3@EXAMPLE.COM','TEST3@example.com'], 
-            ['test4@example.COM','test4@example.com'], 
+            ['test1@EXAMPLE.com', 'test1@example.com'], 
+            ['Test2@Example.com', 'Test2@example.com'], 
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'], 
+            ['test4@example.COM', 'test4@example.com'], 
         ]
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
